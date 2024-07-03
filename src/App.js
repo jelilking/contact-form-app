056b3e56b52a2b5997bf33ce1d94cce2a0731737
jelilking <abduljelilozigi@gmail.com>
2024-07-03 17:20:09 +0100

    intergrateing the state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Result />
+        <h1>Contact Us</h1>
+        <ContactForm
+          handleSubmit={handleSubmit}
+          firstName={firstName}
+          setFirstName={setFirstName}
+          lastName={lastName}
+          setLastName={setLastName}
+          email={email}
+          setEmail={setEmail}
+          queryType={queryType}
+          setQueryType={setQueryType}
+          message={message}
+          setMessage={setMessage}
+          consent={consent}
+          setConsent={setConsent}
+          error={error}
+          setError={setError}
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+function ContactForm({
+  handleSubmit,
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+}) {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-name">
+          <div>
+            <label>First Name *</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label>Last Name *</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="form-email">
+          <label>Email *</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>Query Type *</label>
+          <div className="radio">
+            <label>
+              <input type="radio" name="queryType" value="query" />
+              General Enquiry
+            </label>
+            <label>
+              <input type="radio" name="queryType" value="query" />
+              Support Request
+            </label>
+          </div>
+        </div>
+        <div className="form-message">
+          <label>Message *</label>
+          <textarea></textarea>
+        </div>
+        <div className="form-consent">
+          <label>
+            <input type="checkbox" /> i consent to being contacted by the team
+          </label>
+        </div>
+        <div className="form-submit">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function Result() {
+  return <div></div>;
+}
