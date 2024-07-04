2e9188f0883c0a5f3055bf4c66672c157f4de6a6
jelilking <abduljelilozigi@gmail.com>
2024-07-04 17:46:31 +0100

    styling the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import { useState } from "react";
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [queryType, setQueryType] = useState("");
+  const [message, setMessage] = useState("");
+  const [consent, setConsent] = useState(false);
+  const [error, setError] = useState({
+    firstName: false,
+    lastName: false,
+    email: false,
+    queryType: false,
+    message: false,
+    consent: false,
+  });
+
+  const [result, setResult] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    const newError = {
+      firstName: !firstName,
+      lastName: !lastName,
+      email: !email,
+      queryType: !queryType,
+      message: !message,
+      consent: !consent,
+    };
+
+    setError(newError);
+
+    if (firstName && lastName && email && queryType && message && consent) {
+      setResult("your info has been successfully sent to us");
+    } else if (
+      newError.firstName ||
+      newError.lastName ||
+      newError.email ||
+      newError.queryType ||
+      newError.message ||
+      !consent
+    ) {
+      return;
+    }
   }
 
   return (
     <div className="App">
       <div className="container">
-        <Result />
+        <Result result={result} />
         <h1>Contact Us</h1>
         <ContactForm
           handleSubmit={handleSubmit}
@@ -38,6 +79,14 @@ function ContactForm({
   setFirstName,
   lastName,
   setLastName,
+  email,
+  setEmail,
+  queryType,
+  setQueryType,
+  message,
+  setMessage,
+  consent,
+  setConsent,
 }) {
   return (
     <div>
@@ -60,28 +109,52 @@ function ContactForm({
         </div>
         <div className="form-email">
           <label>Email *</label>
-          <input type="email" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label>Query Type *</label>
           <div className="radio">
             <label>
-              <input type="radio" name="queryType" value="query" />
+              <input
+                type="radio"
+                name="queryType"
+                value="general enquiry"
+                checked={queryType === "general enquiry"}
+                onChange={(e) => setQueryType(e.target.value)}
+              />
               General Enquiry
             </label>
             <label>
-              <input type="radio" name="queryType" value="query" />
+              <input
+                type="radio"
+                name="queryType"
+                value="support request"
+                checked={queryType === "support request"}
+                onChange={(e) => setQueryType(e.target.value)}
+              />
               Support Request
             </label>
           </div>
         </div>
         <div className="form-message">
           <label>Message *</label>
-          <textarea></textarea>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
         <div className="form-consent">
           <label>
-            <input type="checkbox" /> i consent to being contacted by the team
+            <input
+              type="checkbox"
+              value={consent}
+              onChange={(e) => setConsent(e.target.value)}
+            />
+            i consent to being contacted by the team
           </label>
         </div>
         <div className="form-submit">
@@ -92,6 +165,6 @@ function ContactForm({
   );
 }
 
-function Result() {
-  return <div></div>;
+function Result({ result }) {
+  return <div>{result}</div>;
 }
